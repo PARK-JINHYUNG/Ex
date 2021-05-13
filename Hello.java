f6711dd6d7fda466c193c2548dd9140d7738d371
jwinyong <82507431+PARK-JINHYUNG@users.noreply.github.com>
2021-05-13 11:51:08 +0900

    두번째 버전
    인사추가

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
               public static void main(String[] args)
               {
                         System.out.println("Hello Welcome to the java world!");
+                        System.out.println("Hello");
               }
 
 }
